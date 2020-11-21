37ada99c53ed21e42a6acf8db7432578768854a4
Nahuelnp <nahuelyad@gmail.com>
2020-11-20 21:21:03 -0300

    nuevo intento

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,6 +156,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(os.path.dirname(BASE_DIR),'static'),)
 
-MEDIA_URL ='/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
